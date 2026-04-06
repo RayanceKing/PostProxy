c6e1b5267d7b0d9eedb760801ad14fe428cf0b4b
RayanceKing <88320297+RayanceKing@users.noreply.github.com>
2026-04-06 12:36:31 +0800

    Initial Commit

---
+++ PostProxyTests/PostProxyTests.swift
@@ -0,0 +1,18 @@
+//
+//  PostProxyTests.swift
+//  PostProxyTests
+//
+//  Created by rayanceking on 2026/4/6.
+//
+
+import Testing
+
+struct PostProxyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
